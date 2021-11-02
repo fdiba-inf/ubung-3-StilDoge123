e8bb8e231006c7064f89da7549da64fd27489f24
Stilian <stildoge123@abv.bg>
2021-11-03 00:01:55 +0200

    numcount

---
+++ src/exercise3/NumberCounter.java
@@ -0,0 +1,33 @@
+package exercise3;
+
+import java.util.Scanner;
+
+public class NumberCounter {
+    public static void main(String[] args) {
+        double sum = 0;
+        double count = 0;
+        double countNeg = 0;
+        double countPos = 0;
+        double avg;
+        double n;
+        do {
+            Scanner sc = new Scanner(System.in);
+            n = sc.nextDouble();
+            if(n==0){
+                break;
+            }
+            sum += n;
+            count++;
+            if (n > 0) {
+                countPos++;
+            } else if (n < 0) {
+                countNeg++;
+            }
+        } while (n != 0);
+        avg = sum / count;
+        System.out.println("Positive numbers: " + countPos);
+        System.out.println("Negative numbers: " + countNeg);
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + avg);
+    }
+}
